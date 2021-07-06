b831a677c76fa699f1dccefe4aeb40a6651b9473
MurielMauch <murielmauch@gmail.com>
2021-07-06 00:21:35 -0300

    feat: improving code arch

--- Project3/agents.py
+++ Project3/agents.py
@@ -1,6 +1,10 @@
-from game import Agent, Directions, Actions
+import math
+import random
+import time
+import util
+
 from featureExtractors import *
-import random,util,time, math
+from game import Agent, Directions, Actions
 
 """
 This first two agents were implemented to help us understand how the codebase worked, based on labs
@@ -9,10 +13,12 @@ available at https://cs.brynmawr.edu/Courses/cs372/fall2017/Lab1.pdf
 The actual solution for the Reinforcement Learning will be at the remaining classes
 """
 
+
 class RandomAgent(Agent):
     """
     The random agent just calls a possible solution randomly
     """
+
     def __init__(self):
         super().__init__()
         self.__location = None
@@ -39,10 +45,10 @@ class ReflexAgent(Agent):
     """
     The reflex agent calls one of possible direction if they have food
     """
+
     def __init__(self):
         super().__init__()
         self.__location = None
-
 
     def choose_random_direction(self, actions_available):
         action = random.choice(actions_available)
@@ -66,16 +72,16 @@ class ReflexAgent(Agent):
 
         for action in actions_available:
             if action == Directions.WEST:
-                if state.hasFood(location[0]-1, location[1]):
+                if state.hasFood(location[0] - 1, location[1]):
                     return Directions.WEST
             elif action == Directions.SOUTH:
-                if state.hasFood(location[0], location[1]-1):
+                if state.hasFood(location[0], location[1] - 1):
                     return Directions.SOUTH
             elif action == Directions.EAST:
-                if state.hasFood(location[0]+1, location[1]):
+                if state.hasFood(location[0] + 1, location[1]):
                     return Directions.EAST
             elif action == Directions.NORTH:
-                if state.hasFood(location[0], location[1]+1):
+                if state.hasFood(location[0], location[1] + 1):
                     return Directions.NORTH
 
         return self.choose_random_direction(actions_available)
@@ -84,10 +90,13 @@ class ReflexAgent(Agent):
 """
 The functions available from now on are the ones used to implement the Q-Values Reinforcement Agent
 """
+
+
 class ReinforcementAgent(Agent):
     """
     The Reinforcement Agent estimates Q-Values (as well as policies) from experience
     """
+
     def __init__(self, actions_available=None, num_training=100, epsilon=0.5, alpha=0.5, gamma=1):
         """
         Instantiates the Reinforcement Agent
@@ -109,23 +118,20 @@ class ReinforcementAgent(Agent):
         self.epsilon = float(epsilon)
         self.alpha = float(alpha)
         self.discount = float(gamma)
+        self.last_state = None
+        self.last_action = None
+        self.episode_rewards = 0.0
 
-    def update(self, state, action, nextState, reward):
-        """
-            This class will call this function, which you write, after
-            observing a transition and reward
-        """
-        util.raiseNotDefined()
+    def update(self, state, action, next_state, reward):
+        pass  # not implemented here
 
-    def getLegalActions(self,state):
+    def getLegalActions(self, state):
         """
-          Get the actions available for a given
-          state. This is what you should use to
-          obtain legal actions for a state
+          Get the actions available for a given state
         """
         return self.actions_available(state)
 
-    def observeTransition(self, state,action,nextState,deltaReward):
+    def observeTransition(self, state, action, next_state, delta_reward):
         """
             Called by environment to inform agent that a transition has
             been observed. This will result in a call to self.update
@@ -133,30 +139,31 @@ class ReinforcementAgent(Agent):
 
             NOTE: Do *not* override or call this function
         """
-        self.episodeRewards += deltaReward
-        self.update(state,action,nextState,deltaReward)
+        self.episode_rewards += delta_reward
+        self.update(state, action, next_state, delta_reward)
 
     def startEpisode(self):
         """
           Called by environment when new episode is starting
+          Making sure we have the default values set
         """
-        self.lastState = None
-        self.lastAction = None
-        self.episodeRewards = 0.0
+        self.last_state = None
+        self.last_action = None
+        self.episode_rewards = 0.0
 
     def stopEpisode(self):
         """
           Called by environment when episode is done
         """
         if self.episodes_so_far < self.num_training:
-              self.accum_train_rewards += self.episodeRewards
+            self.accum_train_rewards += self.episode_rewards
         else:
-              self.accum_test_rewards += self.episodeRewards
+            self.accum_test_rewards += self.episode_rewards
         self.episodes_so_far += 1
         if self.episodes_so_far >= self.num_training:
-          # Take off the training wheels
-          self.epsilon = 0.0    # no exploration
-          self.alpha = 0.0      # no learning
+            # Take off the training wheels
+            self.epsilon = 0.0  # no exploration
+            self.alpha = 0.0  # no learning
 
     def isInTraining(self):
         return self.episodes_so_far < self.num_training
@@ -173,62 +180,60 @@ class ReinforcementAgent(Agent):
     def setDiscount(self, discount):
         self.discount = discount
 
-    def doAction(self,state,action):
+    def doAction(self, state, action):
         """
             Called by inherited class when
             an action is taken in a state
         """
-        self.lastState = state
-        self.lastAction = action
+        self.last_state = state
+        self.last_action = action
 
     ###################
     # Pacman Specific #
     ###################
     def observationFunction(self, state):
         """
-            This is where we ended up after our last action.
-            The simulation should somehow ensure this is called
+            Called right after the last action
         """
-        if not self.lastState is None:
-            reward = state.getScore() - self.lastState.getScore()
-            self.observeTransition(self.lastState, self.lastAction, state, reward)
+        if not self.last_state is None:
+            reward = state.getScore() - self.last_state.getScore()
+            self.observeTransition(self.last_state, self.last_action, state, reward)
         return state
 
     def registerInitialState(self, state):
         self.startEpisode()
         if self.episodes_so_far == 0:
-            print("Beginning {} episodes of Training", self.num_training)
+            print("Beginning {} episodes of Training".format(self.num_training))
 
     def final(self, state):
         """
-          Called by Pacman game at the terminal state
+          Ends the game
         """
-        deltaReward = state.getScore() - self.lastState.getScore()
-        self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
+        delta_reward = state.getScore() - self.last_state.getScore()
+        self.observeTransition(self.last_state, self.last_action, state, delta_reward)
         self.stopEpisode()
 
-        # Make sure we have this var
         if not 'episodeStartTime' in self.__dict__:
             self.episodeStartTime = time.time()
         if not 'lastWindowAccumRewards' in self.__dict__:
-            self.lastWindowAccumRewards = 0.0
-        self.lastWindowAccumRewards += state.getScore()
+            self.last_window_accum_rewards = 0.0
+        self.last_window_accum_rewards += state.getScore()
 
-        NUM_EPS_UPDATE = 100
-        if self.episodes_so_far % NUM_EPS_UPDATE == 0:
+        num_eps_update = 100
+        if self.episodes_so_far % num_eps_update == 0:
             print("Reinforcement Learning Status:")
-            windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
+            window_avg = self.last_window_accum_rewards / float(num_eps_update)
             if self.episodes_so_far <= self.num_training:
-                trainAvg = self.accum_train_rewards / float(self.episodes_so_far)
+                train_avg = self.accum_train_rewards / float(self.episodes_so_far)
                 print("\tCompleted {} out of {} training episodes".format(self.episodes_so_far, self.num_training))
-                print("\tAverage Rewards over all training: {}", trainAvg)
+                print("\tAverage Rewards over all training: {}".format(train_avg))
             else:
-                testAvg = float(self.accum_test_rewards) / (self.episodes_so_far - self.num_training)
-                print("\nCompleted {} test episodes", self.episodes_so_far - self.num_training)
-                print("\nAverage Rewards over testing: {}", testAvg)
-            print("\nAverage Rewards for last {} episodes: {}".format(NUM_EPS_UPDATE, windowAvg))
-            print("\nEpisode took {} seconds", time.time() - self.episodeStartTime)
-            self.lastWindowAccumRewards = 0.0
+                test_avg = float(self.accum_test_rewards) / (self.episodes_so_far - self.num_training)
+                print("\nCompleted {} test episodes".format(self.episodes_so_far - self.num_training))
+                print("\nAverage Rewards over testing: {}".format(test_avg))
+            print("\nAverage Rewards for last {} episodes: {}".format(num_eps_update, window_avg))
+            print("\nEpisode took {} seconds".format(time.time() - self.episodeStartTime))
+            self.last_window_accum_rewards = 0.0
             self.episodeStartTime = time.time()
 
         if self.episodes_so_far == self.num_training:
@@ -244,7 +249,7 @@ class QLearningAgent(ReinforcementAgent):
     def __init__(self, **args):
         ReinforcementAgent.__init__(self, **args)
 
-        self.qValues = util.Counter()
+        self.q_values = util.Counter()
         print("ALPHA: {}".format(self.alpha))
         print("DISCOUNT: {}".format(self.discount))
         print("EXPLORATION: {}".format(self.epsilon))
@@ -252,119 +257,69 @@ class QLearningAgent(ReinforcementAgent):
     def getQValue(self, state, action):
         """
           Returns Q(state,action)
-          Should return 0.0 if we never seen
-          a state or (state,action) tuple
+          Should return 0.0 if we never seen a state or (state,action) tuple
         """
-        "*** YOUR CODE HERE ***"
-        return self.qValues[(state, action)]
+        return self.q_values[(state, action)]
 
     def getValue(self, state):
         """
-          Returns max_action Q(state,action)
-          where the max is over legal actions.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
+          Returns max_action Q(state,action) where the max is over legal actions. 
+          Return 0.0 if final state
         """
-        "*** YOUR CODE HERE ***"
-        possibleStateQValues = util.Counter()
+        possible_state_q_values = util.Counter()
         for action in self.getLegalActions(state):
-            possibleStateQValues[action] = self.getQValue(state, action)
+            possible_state_q_values[action] = self.getQValue(state, action)
 
-        return possibleStateQValues[possibleStateQValues.argMax()]
+        return possible_state_q_values[possible_state_q_values.argMax()]
 
     def getPolicy(self, state):
         """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
+          Compute the best action to take in a state.  
+          Returns none if final state
         """
-        "*** YOUR CODE HERE ***"
-        possibleStateQValues = util.Counter()
-        possibleActions = self.getLegalActions(state)
-        if len(possibleActions) == 0:
+        possible_state_q_values = util.Counter()
+        possible_actions = self.getLegalActions(state)
+        if len(possible_actions) == 0:
             return None
 
-        for action in possibleActions:
-            possibleStateQValues[action] = self.getQValue(state, action)
+        for action in possible_actions:
+            possible_state_q_values[action] = self.getQValue(state, action)
 
-        if possibleStateQValues.totalCount() == 0:
-            return random.choice(possibleActions)
+        if possible_state_q_values.totalCount() == 0:
+            return random.choice(possible_actions)
         else:
-            return possibleStateQValues.argMax()
+            return possible_state_q_values.argMax()
 
     def getAction(self, state):
         """
-          Compute the action to take in the current state.  With
-          probability self.epsilon, we should take a random action and
-          take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
-
-          HINT: You might want to use util.flipCoin(prob)
-          HINT: To pick randomly from a list, use random.choice(list)
+          Compute the action to take in the current state.  
+          With probability self.epsilon >> take a random action 
+          otherwise >> take the best policy action 
+          None if terminal state
         """
-        # Pick Action
-        legalActions = self.getLegalActions(state)
+        legal_actions = self.getLegalActions(state)
         action = None
-        "*** YOUR CODE HERE ***"
-        if len(legalActions) > 0:
+        if len(legal_actions) > 0:
             if util.flipCoin(self.epsilon):
-                action = random.choice(legalActions)
+                action = random.choice(legal_actions)
             else:
                 action = self.getPolicy(state)
 
         return action
 
-    def update(self, state, action, nextState, reward):
+    def update(self, state, action, next_state, reward):
         """
-          The parent class calls this to observe a
-          state = action => nextState and reward transition.
-          You should do your Q-Value update here
-
-          NOTE: You should never call this function,
-          it will be called on your behalf
+          The parent class calls this to observe a state = action => nextState and reward transition.
+          Here is where we update our Q-Value
         """
-        "*** YOUR CODE HERE ***"
-        print("State: {}. Action: {}. NextState: {}. Reward: {}".format(state, action, nextState, reward))
-        print("QVALUE: {}", self.getQValue(state, action))
-        print("VALUE: {}", self.getValue(nextState))
-        self.qValues[(state, action)] = self.getQValue(state, action) + self.alpha * (
-                    reward + self.discount * self.getValue(nextState) - self.getQValue(state, action))
+        print("State: {}. Action: {}. NextState: {}. Reward: {}".format(state, action, next_state, reward))
+        print("QVALUE: {}".format(self.getQValue(state, action)))
+        print("VALUE: {}".format(self.getValue(next_state)))
+        self.q_values[(state, action)] = self.getQValue(state, action) + self.alpha * (
+                reward + self.discount * self.getValue(next_state) - self.getQValue(state, action))
 
 
-class PacmanQAgent(QLearningAgent):
-    "Exactly the same as QLearningAgent, but with different default parameters"
-
-    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, num_training=0, **args):
-        """
-        These default parameters can be changed from the pacman.py command line.
-        For example, to change the exploration rate, try:
-            python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
-
-        alpha    - learning rate
-        epsilon  - exploration rate
-        gamma    - discount factor
-        num_training - number of training episodes, i.e. no learning after these many episodes
-        """
-        args['epsilon'] = epsilon
-        args['gamma'] = gamma
-        args['alpha'] = alpha
-        args['num_training'] = num_training
-        self.index = 0  # This is always Pacman
-        QLearningAgent.__init__(self, **args)
-
-    def getAction(self, state):
-        """
-        Simply calls the getAction method of QLearningAgent and then
-        informs parent of action for Pacman.  Do not change or remove this
-        method.
-        """
-        action = QLearningAgent.getAction(self, state)
-        self.doAction(state, action)
-        return action
-
-
-class ApproximateQAgent(PacmanQAgent):
+class ApproximateQAgent(QLearningAgent):
     """
        ApproximateQLearningAgent
 
@@ -373,12 +328,15 @@ class ApproximateQAgent(PacmanQAgent):
        should work as is.
     """
 
-    def __init__(self, extractor='IdentityExtractor', **args):
+    def __init__(self, extractor='IdentityExtractor', epsilon=0.05, gamma=0.8, alpha=0.2, num_training=0, **args):
         self.featExtractor = util.lookup(extractor, globals())()
-        PacmanQAgent.__init__(self, **args)
 
-        # You might want to initialize weights here.
-        "*** YOUR CODE HERE ***"
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['num_training'] = num_training
+        self.index = 0  # Pacman initial position
+        QLearningAgent.__init__(self, **args)
         self.weights = util.Counter()
 
     def getQValue(self, state, action):
@@ -393,24 +351,34 @@ class ApproximateQAgent(PacmanQAgent):
             qValue += (self.weights[key] * features[key])
         return qValue
 
-    def update(self, state, action, nextState, reward):
+    def getAction(self, state):
+        """
+        Simply calls the getAction method of QLearningAgent and then
+        informs parent of action for Pacman.  Do not change or remove this
+        method.
+        """
+        action = QLearningAgent.getAction(self, state)
+        self.doAction(state, action)
+        return action
+
+    def update(self, state, action, next_state, reward):
         """
            Should update your weights based on transition
         """
         "*** YOUR CODE HERE ***"
         features = self.featExtractor.getFeatures(state, action)
-        possibleStateQValues = []
+        possible_state_q_values = []
         for act in self.getLegalActions(state):
-            possibleStateQValues.append(self.getQValue(state, act))
+            possible_state_q_values.append(self.getQValue(state, act))
         for key in features.keys():
             self.weights[key] += self.alpha * (reward + self.discount * (
-                        (1 - self.epsilon) * self.getValue(nextState) + (self.epsilon / len(possibleStateQValues)) * (
-                    sum(possibleStateQValues))) - self.getQValue(state, action)) * features[key]
+                    (1 - self.epsilon) * self.getValue(next_state) + (self.epsilon / len(possible_state_q_values)) * (
+                sum(possible_state_q_values))) - self.getQValue(state, action)) * features[key]
 
     def final(self, state):
         "Called at the end of each game."
         # call the super-class final method
-        PacmanQAgent.final(self, state)
+        QLearningAgent.final(self, state)
 
         # did we finish training?
         if self.episodes_so_far == self.num_training:
